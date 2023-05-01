5b4a640863225bcb41ba545e3d8fe6a466debf22
Jensevent <jens.heesbeen@truelime.nl>
2023-05-01 11:32:30 +0200

    Strapi

---
+++ local_strapi/local_strapi/src/api/car-dealer/controllers/car-dealer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * car-dealer controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::car-dealer.car-dealer');
